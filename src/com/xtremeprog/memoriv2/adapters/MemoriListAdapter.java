96cdda1a0fe5b8b491bf4641f4faa26aaf197d69
Robert <rzheng@xtremeprog.com>
2012-11-27 17:28:58 +0800

    * implement show qrcode button

--- src/com/xtremeprog/memoriv2/adapters/MemoriListAdapter.java
+++ src/com/xtremeprog/memoriv2/adapters/MemoriListAdapter.java
@@ -8,10 +8,13 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -19,12 +22,22 @@ import com.xtremeprog.memoriv2.R;
 import com.xtremeprog.memoriv2.models.Memori;
 
 public class MemoriListAdapter extends BaseAdapter {
+	
+	private class ViewHolder {
+
+		public TextView txt_photo_url;
+		public ImageView img_photo;
+		public TextView txt_count;
+		public Button btn_qrcode;
+		public Memori memori;}
 
 	private final String TAG = MemoriListAdapter.class.getSimpleName();
 	private ArrayList<Memori> lst_memori;
 	private ImageLoader img_loader;
+	private Context context;
 	
 	public MemoriListAdapter(Context context) {
+		this.context = context;
 		lst_memori = new ArrayList<Memori>();
 		img_loader = ImageLoader.getInstance();
 		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
@@ -77,19 +90,40 @@ public class MemoriListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewGroup view = (convertView instanceof ViewGroup) ? (ViewGroup) convertView
-				: (ViewGroup) LayoutInflater.from(parent.getContext()).inflate(
-						R.layout.memori_item, null);
+		ViewHolder holder;
+		if ( convertView == null ) {
+			convertView =  LayoutInflater.from(parent.getContext()).inflate(
+					R.layout.memori_item, null);
+			holder = buildTag(convertView); 
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}	
 		
 		Memori memori = (Memori) getItem(position);
-		TextView txt_photo_url = (TextView) view.findViewById(R.id.txt_photo_url);
-		txt_photo_url.setText(memori.get_cover());
-		ImageView img_photo = (ImageView) view.findViewById(R.id.img_photo);
-		img_loader.displayImage("file://" + memori.get_cover(), img_photo);
-		TextView txt_count = (TextView) view.findViewById(R.id.txt_count);
-		txt_count.setText(memori.get_photo_count() + "");
+		holder.memori = memori;
+		holder.txt_photo_url.setText(memori.get_cover());
+		img_loader.displayImage("file://" + memori.get_cover(), holder.img_photo);
+		holder.txt_count.setText(memori.get_photo_count() + "");
 		
-		return view;
+		return convertView;
+	}
+
+	private ViewHolder buildTag(View convertView) {
+		final ViewHolder holder;
+		holder = new ViewHolder();
+		holder.txt_photo_url = (TextView) convertView.findViewById(R.id.txt_photo_url);
+		holder.img_photo = (ImageView) convertView.findViewById(R.id.img_photo);
+		holder.txt_count = (TextView) convertView.findViewById(R.id.txt_count);
+		holder.btn_qrcode = (Button) convertView.findViewById(R.id.btn_qrcode);
+		holder.btn_qrcode.setOnClickListener(new View.OnClickListener() { 
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MemoriListAdapter.this.context, holder.memori.get_photo_count() + "", Toast.LENGTH_SHORT).show();
+			}
+		});
+		return holder;
 	}
 
 }
