28a3b0a502fee12fe38362dbf31a1ae81a03bbcd
Robert <rzheng@xtremeprog.com>
2012-11-27 14:41:32 +0800

    * use universal-image-loader to load image

--- src/com/xtremeprog/memoriv2/adapters/MemoriCursorAdapter.java
+++ src/com/xtremeprog/memoriv2/adapters/MemoriCursorAdapter.java
@@ -2,19 +2,21 @@ package com.xtremeprog.memoriv2.adapters;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.ThumbnailUtils;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 public class MemoriCursorAdapter extends SimpleCursorAdapter {
 
-	private static int THUMBSIZE = 80;
+	private ImageLoader img_loader;
 	
 	public MemoriCursorAdapter(Context context, int layout, Cursor c,
 			String[] from, int[] to, int flags) {
 		super(context, layout, c, from, to, flags);
+		img_loader = ImageLoader.getInstance();
+		img_loader.init(ImageLoaderConfiguration.createDefault(context));
 	}
 
 	@Override
@@ -22,8 +24,7 @@ public class MemoriCursorAdapter extends SimpleCursorAdapter {
         try {
             v.setImageResource(Integer.parseInt(value));
         } catch (NumberFormatException nfe) {
-        	Bitmap ThumbImage = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(value), THUMBSIZE, THUMBSIZE);
-            v.setImageBitmap(ThumbImage);
+        	img_loader.displayImage("file://" + value, v);
         }
 	}
 	
